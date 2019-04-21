a38bdf19839fc757619651d6524b265deea0e983
Jerry Ma <jerrymax.jm@outlook.com>
2019-04-21 22:33:05 +0800

    update solutions

--- solutions/0942.di-string-match/di-string-match.cpp
+++ solutions/0942.di-string-match/di-string-match.cpp
@@ -14,7 +14,7 @@
  * Given a string S that only contains "I" (increase) or "D" (decrease), let N
  * = S.length.
  *
- * Return any permutation A of [0, 1, ..., N] such that for all i = 0,?...,
+ * Return any permutation A of [0, 1, ..., N] such that for all i = 0, ...,
  * N-1:
  *
  *
